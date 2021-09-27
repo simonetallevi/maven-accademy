d085c22ab14e053300b9f2b6e7d9941a4267e6e1
simonetallevi <simone.tallevi@gmail.com>
2021-09-27 10:47:45 +0200

    6

---
+++ 6-scope/src/main/java/com/maven/scope/App.java
@@ -0,0 +1,12 @@
+package com.maven.scope;
+
+import lombok.Data;
+
+@Data
+public class App{
+    private static String test;
+
+    public static void main(String[] args) {
+
+    }
+}
--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 ##3 maven dependencies
 
 ##4 maven modules
+
+##5 lib installation
+
+##6 Maven scope
+
+##7
